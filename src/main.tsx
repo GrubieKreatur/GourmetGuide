f357d0a1a72ad5ca92a75690c8b7b5faea20e99b
GrubieKreatur <121688796+GrubieKreatur@users.noreply.github.com>
2024-10-04 22:49:19 +0200

    test chatt gpt

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,19 @@
 
 
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+if (rootElement) {
+    const root = createRoot(rootElement);
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+} else {
+    console.error('Root element not found');
+}
